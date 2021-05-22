8fb7e0e93a842a72f5039808e84439e7bbb88184
ayanda qoseem <kayceayanda11@gmail.com>
2021-05-22 04:56:20 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+const navItemContainer = document.getElementById("nav-item-wrapper")
+
+// FUNCTION THAT OPEN THE NAVBAR
+function toggleNavbar(){
+    navItemContainer.classList.toggle("open-nav-items")
+}
